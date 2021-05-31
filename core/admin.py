3f7379e63e30ff5bfc5ef19ac436c22fa9f6722f
Arjun Singh <admin@arjunsingh.com.np>
2021-06-01 01:38:59 +0545

    Customized admin panel for core app

--- core/admin.py
+++ core/admin.py
@@ -7,5 +7,13 @@ class BloodAdmin(ModelAdmin):
     list_display = ['group', 'slug']
 
 
-admin.site.register(models.Request)
+class RequestAdmin(ModelAdmin):
+    list_display = ['requested_by', 'blood_group', 'for_date', 'donated_by', 'status']
+    list_filter = ['status', 'blood_group']
+    date_hierarchy = 'for_date'
+    search_fields = ['requested_by__email', 'donated_by__email','requested_by__phone_number', 'donated_by__phone_number', 'district', 'local_level']
+    list_per_page = 100
+
+
+admin.site.register(models.Request, RequestAdmin)
 admin.site.register(models.Blood, BloodAdmin)
